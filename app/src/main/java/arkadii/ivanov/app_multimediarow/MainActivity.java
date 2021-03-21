b3c22d9107ef251fad94a9512d4422d98a412b60
vasia18 <studiovasia18@gmail.com>
2021-03-22 01:07:30 +0300

    по нажатию на кнопку появляется звук

---
+++ app/src/main/java/arkadii/ivanov/app_multimediarow/MainActivity.java
@@ -0,0 +1,14 @@
+package arkadii.ivanov.app_multimediarow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
