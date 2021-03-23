3a9107a18e9b85ebf914130a5545c13c9190dcad
vasia18 <studiovasia18@gmail.com>
2021-03-23 21:59:27 +0300

    Код как подключать звуковой файл на кнопку

--- app/src/main/java/arkadii/ivanov/app_multimediarow/MainActivity.java
+++ app/src/main/java/arkadii/ivanov/app_multimediarow/MainActivity.java
@@ -2,7 +2,11 @@ package arkadii.ivanov.app_multimediarow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button button = (Button) findViewById(R.id.button);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Context context;
+				MediaPlayer md = MediaPlayer.create(MainActivity.this, R.raw.muv_1);
+				md.start();
+			}
+		});
+
 	}
 }
